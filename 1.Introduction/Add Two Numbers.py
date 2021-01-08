ca385e3220de73bee112c494f5f89c8ef894d4e6
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-01-08 08:05:18 +0530

    Add files via upload

---
+++ 1.Introduction/Add Two Numbers.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  8 07:47:18 2021
+
+@author: Aripandi
+"""
+
+#program to add two numbers
+
+#get the values from user
+
+n1 = input('Enter the First number: ')
+n2 = input('Enter the Second number: ')
+
+#Add the number
+n3 = int(n1)+int(n2)
+
+#Display the Value
+print('Sum of {} and {} is {}'.format(n1,n2,n3))
