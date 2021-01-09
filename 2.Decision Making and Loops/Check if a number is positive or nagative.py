1454e07d44cd4e0cf3d4c1e73325683bb4cb9cb8
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-01-09 08:20:24 +0530

    Add files via upload

---
+++ 2.Decision Making and Loops/Check if a number is positive or nagative.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  9 08:09:32 2021
+
+@author: Aripandi
+"""
+
+num = int(input('Enter the Number: '))
+
+if num < 0:
+    print('Number is Negative')
+elif num == 0:
+    print('Number is Zero')
+else:
+    print('Number is Positive')
