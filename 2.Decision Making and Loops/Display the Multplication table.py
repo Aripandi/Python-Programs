0e61ac531c616d2b5180cc9407e0115a135a6a33
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-01-10 12:02:40 +0530

    Add files via upload

---
+++ 2.Decision Making and Loops/Display the Multplication table.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 10 07:58:30 2021
+
+@author: Aripandi
+"""
+
+n = int(input('Display the table of: '))
+
+for i in range(1,11):
+    print(n,'x',i,'=',n*i)
