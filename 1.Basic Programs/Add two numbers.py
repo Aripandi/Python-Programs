61d3ab21967e61fbd15ca716090c2054d7b20312
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-05-13 08:25:32 +0530

    Create Add two numbers.py

---
+++ 1.Basic Programs/Add two numbers.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter no 1: "))
+num2 = int(input("Enter no 2: "))
+
+add = num1+num2
+
+print("Added number: ",add)
+
+#Another type
+
+a = input("Enter No1: ")
+b = input("Enter No2: ")
+
+sum = float(a)+float(b)
+
+print("The sum of {0} and {1} = {2}".format(a,b,sum))
