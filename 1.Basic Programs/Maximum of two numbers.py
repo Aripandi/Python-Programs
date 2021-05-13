235f3b942de0d8bd27788958da02a82c539967fe
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-05-13 08:35:17 +0530

    Create Maximum of two numbers.py

---
+++ 1.Basic Programs/Maximum of two numbers.py
@@ -0,0 +1,19 @@
+def maximum(a,b):
+    if(a>b):
+        return a
+    else:
+        return b
+    
+    
+maxi = maximum(3, 4)
+
+print("Maximum: ",maxi)
+
+
+#using max() function
+
+num1 = int(input("Entrer NO1: "))
+num2 = int(input("Enter NO2: "))
+
+maximum = max(num1,num2)
+print("maximu: ",maximum) 
